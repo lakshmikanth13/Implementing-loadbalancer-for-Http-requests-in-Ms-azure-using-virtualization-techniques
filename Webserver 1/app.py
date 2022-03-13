14d687df1d0ee7e8a9bd6bd943db159964d431eb
lakshmikanth13 <68058212+lakshmikanth13@users.noreply.github.com>
2022-03-13 14:15:43 +0100

    Add files via upload

---
+++ Webserver 1/app.py
@@ -0,0 +1,20 @@
+rom flask import Flask, render_template, request, send_file, redirect, url_for, session
+import os, psutil, json, time, subprocess, sys
+import socket
+
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    html = "<h3>Hello {name}!</h3> <b>Hostname:</b> {hostname} <h3> This is from VM8 </h3> <br/>"
+    return html.format(name=os.getenv("NAME", "Kurt"), hostname=socket.gethostname())
+
+@app.route("/load")
+def load():
+    load=psutil.cpu_percent()
+#    html = "<h4>Hello bro....check percentage ....{chudu}"
+    return str(load) #html.format(chudu=load)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=4000)
+
+
